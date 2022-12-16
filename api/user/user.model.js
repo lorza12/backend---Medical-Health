eeef8eef00c99c0de0771033ef8056448eb5ffab
juan_lorza <lorza112@hotmail.com>
2022-12-16 00:12:01 -0500

    deleted file

--- api/user/user.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const UserSchema = new mongoose_1.Schema({});
-const User = (0, mongoose_1.model)('User', UserSchema);
-exports.default = User;
