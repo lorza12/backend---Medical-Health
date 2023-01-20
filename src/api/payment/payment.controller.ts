f2b06093524f2ac002685591a83a2c57edd9af6b
juan_lorza <lorza112@hotmail.com>
2023-01-20 15:04:02 -0500

    implement pay

--- src/api/payment/payment.controller.ts
+++ src/api/payment/payment.controller.ts
@@ -1,19 +1,30 @@
 import { Request, Response } from "express";
-import User from "../user/user.model";
-import Payment from "./payment.model";
+import Stripe from 'stripe';
+
+const SECRET_KEY = process.env.STRIPE_SECRET_KEY as string
+const stripe = new Stripe(SECRET_KEY, {apiVersion:'2022-11-15'} )
 
 
-
-
-export async function handlePayment(req: Request, res: Response) {
+ async function handlePayment(req: Request, res: Response) {
     const { paymentMethod, amount } = req.body;
+    console.log(req.body)
     try {
-        const { id, card } = paymentMethod;
+        const { id, card } = paymentMethod
+         const payment = await stripe.paymentIntents.create({
+           payment_method: id,
+           amount,
+           currency: "usd",
+           confirm: true,
+           description: 'pago de prueba'
+         })
         
 
-      return res.json(Payment);
+      return res.json({ message: 'success', payment });
     } catch (error) {
+      console.log(error)
       return res.status(500).json(error);
+      
     }
   }
-  
+
+export default handlePayment;
